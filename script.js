c3a9bfa92f1209b6321ca623bc7a8c4d443c68bd
CKCK666 <vishnuck464@gmail.com>
2021-09-29 20:35:22 +0530

    finish

---
+++ script.js
@@ -0,0 +1,39 @@
+function resize1(){
+    document.getElementById('image1').style.display='none';
+    document.getElementById('iframe1').src+='?autoplay=1';
+    var a=document.getElementById('video1');
+    a.style.display='block';
+    
+    
+}
+function resize2(){
+    var y=document.getElementById('video2');
+    y.style.display='block';
+    y.src+='?autoplay=1';
+    document.getElementById('pic2').style.display='none'; 
+}
+
+function resize3(){
+    document.getElementById('image3').style.display='none';
+    var z=document.getElementById('video3');
+    z.style.display='block';
+    document.getElementById('iframe3').src+='?autoplay=1';
+    
+}
+
+function resize4(){
+    document.getElementById('image4').style.display='none';
+    var a=document.getElementById('video4');
+    a.style.display='block';
+    document.getElementById('iframe4').src+='?autoplay=1';
+    
+}
+
+function resize5(){
+    document.getElementById('image5').style.display='none';
+    document.getElementById('iframe5').src+='?autoplay=1';
+    var a=document.getElementById('video5');
+    a.style.display='block';
+    
+    
+}
